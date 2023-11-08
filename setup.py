a1a04388944c3c2e2d657bc571d6cf241101e865
Andrea de la Huerta <info@metamorfosys.de>
2023-11-08 16:11:53 +0100

    Upgrade dependencies to support python3 and django <4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-multinavigation',
-    version='1.4',
+    version='1.5',
     description='A Django app to easily generate navigation and breadcrumbs',
     long_description='A simple, flexible and DRY way to define and create navigations (tabnavigation and breadcrumbs) from the urlpatterns and a context-processor.',
     long_description_content_type='text/x-rst',
@@ -15,6 +15,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -22,15 +23,12 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    python_requires='>=2.7, >=3.5.*, <4',
+    python_requires='>=2.7, >=3, <4',
     project_urls={
         'Bug Reports': 'https://github.com/adlh/django-multinavigation',
         'Source': 'https://github.com/adlh/django-multinavigation',
